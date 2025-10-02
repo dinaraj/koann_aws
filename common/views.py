26c4e5d696989898230a17305d430ef5a5f38957
Dina Rajaonson <d.rajaonson@gmail.com>
2025-10-02 21:54:52 +0400

    Login page Remove login_required

--- common/views.py
+++ common/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 
 
-@login_required
 def index(request):
     if request.user.is_staff:
         return redirect('jobs:job_list')
